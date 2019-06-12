5016ddf9b89108bdfa30cfa330fbb1181f709f77
James Nutter <nutterj90@gmail.com>
2019-06-12 14:44:39 -0400

    style: add more styling to form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import styles from './App.module.scss';
 const App = () => {
   return (
     <div className={styles.App}>
+      <img src="https://i.imgur.com/ozG3WX6.png" alt="Flexed Arm" width="200" />
       <h1>ShuffleFit</h1>
       <UserForm />
     </div>
