8f5bc6cd9f949d2a1cd344e3dbc53ac439cca0a3
Jeff <stroup19.j@gmail.com>
2019-06-11 23:16:40 -0400

    fixed dups and cleaned if block up a bit

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -67,63 +67,65 @@ export class UserForm extends Component {
 
   handleSubmit = () => {
     const { workoutType, exerciseNumber } = this.state;
-
+    let exerciseTempArray = [];
+    let i = 0;
     if (workoutType === 'Chest') {
-      for (let i = 0; i < exerciseNumber; i++) {
+      while( i < exerciseNumber ) {
         let exercise = chest[Math.floor(Math.random() * chest.length)];
-        this.state.workout.includes(exercise.name)
-          ? console.log('Found duplicates')
-          : this.setState(prevState => ({
-              workout: [...prevState.workout, exercise.name]
-            }));
+        if(!this.checkDups(exerciseTempArray, exercise.name)){
+          exerciseTempArray = exerciseTempArray.concat(exercise.name);
+          i++;
+        }
       }
     } else if (workoutType === 'Back') {
-      for (let i = 0; i < exerciseNumber; i++) {
+      while( i < exerciseNumber ) {
         let exercise = back[Math.floor(Math.random() * back.length)];
-        this.state.workout.includes(exercise.name)
-          ? console.log('Found duplicates')
-          : this.setState(prevState => ({
-              workout: [...prevState.workout, exercise.name]
-            }));
+        if(!this.checkDups(exerciseTempArray, exercise.name)){
+          exerciseTempArray = exerciseTempArray.concat(exercise.name);
+          i++;
+        }
       }
     } else if (workoutType === 'Shoulders') {
-      for (let i = 0; i < exerciseNumber; i++) {
+      while( i < exerciseNumber ) {
         let exercise = shoulders[Math.floor(Math.random() * shoulders.length)];
-        this.state.workout.includes(exercise.name)
-          ? console.log('Found duplicates')
-          : this.setState(prevState => ({
-              workout: [...prevState.workout, exercise.name]
-            }));
+        if(!this.checkDups(exerciseTempArray, exercise.name)){
+          exerciseTempArray = exerciseTempArray.concat(exercise.name);
+          i++;
+        }
       }
     } else if (workoutType === 'Legs') {
-      for (let i = 0; i < exerciseNumber; i++) {
+      while( i < exerciseNumber ) {
         let exercise = legs[Math.floor(Math.random() * legs.length)];
-        this.state.workout.includes(exercise.name)
-          ? console.log('Found duplicates')
-          : this.setState(prevState => ({
-              workout: [...prevState.workout, exercise.name]
-            }));
+        if(!this.checkDups(exerciseTempArray, exercise.name)){
+          exerciseTempArray = exerciseTempArray.concat(exercise.name);
+          i++;
+        }
       }
     } else if (workoutType === 'Arms') {
-      for (let i = 0; i < exerciseNumber; i++) {
+      while( i < exerciseNumber ) {
         let exercise = arms[Math.floor(Math.random() * arms.length)];
-        this.state.workout.includes(exercise.name)
-          ? console.log('Found duplicates')
-          : this.setState(prevState => ({
-              workout: [...prevState.workout, exercise.name]
-            }));
+        if(!this.checkDups(exerciseTempArray, exercise.name)){
+          exerciseTempArray = exerciseTempArray.concat(exercise.name);
+          i++;
+        }
       }
-    } else {
-      for (let i = 0; i < exerciseNumber; i++) {
+    } else {    
+      while( i < exerciseNumber ) {
         let exercise = abs[Math.floor(Math.random() * abs.length)];
-        this.state.workout.includes(exercise.name)
-          ? console.log('Found duplicates')
-          : this.setState(prevState => ({
-              workout: [...prevState.workout, exercise.name]
-            }));
+        if(!this.checkDups(exerciseTempArray, exercise.name)){
+          exerciseTempArray = exerciseTempArray.concat(exercise.name);
+          i++;
+        }
       }
     }
+    this.setState({
+      workout: exerciseTempArray,
+    }); 
   };
+
+  checkDups(arr, exercise){
+    return arr.includes(exercise) ? 1 : 0;
+  }
 
   render() {
     const { step, name, workoutType, exerciseNumber, workout } = this.state;
